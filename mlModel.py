394c29d09ff314a056978136a543f743b196822f
Adit Deshpande <adeshpande3@ucla.edu>
2017-06-06 09:48:27 -0700

    First commit

---
+++ mlModel.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+import numpy as np 
+
+# Load in the data
+properties = pd.read_csv('properties_2016.csv')
+train = pd.read_csv('train_2016.csv')
+sampleSub = pd.read_csv('sample_submission.csv')
+
+# Preprocess the data
+
+# Train the model
+
+# Get the predictions
+
+
